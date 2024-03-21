a9695a28c3240c9038f083fdedeb9c8072414c85
Ryan <rxyuan887@gmail.com>
2024-03-20 17:07:57 -0700

    light working

--- light-meter.py
+++ light-meter.py
@@ -2,16 +2,16 @@
 
 # https://forums.raspberrypi.com/viewtopic.php?t=112888
 
-import picamera
-import picamera.array
+import picamera2
+# import picamera2.array
 import numpy as np
 from time import sleep
 
 
 def main():
-    with picamera.PiCamera() as camera:
+    with picamera2.Picamera2() as camera:
         camera.resolution = (320, 240)
-        with picamera.array.PiRGBArray(camera) as stream:
+        with camera.capture_array() as stream:
             camera.exposure_mode = 'auto'
             camera.awb_mode = 'auto'
             print("Initializing Pi Camera")
@@ -19,7 +19,7 @@ def main():
             camera.exposure_mode = 'off'
             while True:
                 try:
-                    camera.capture(stream, format='rgb')
+                    camera.capture(stream, format='r')
                     # pixAverage = int(np.average(stream.array[...,1]))
                     pixAverage = np.average(stream.array[...,1])
                     print ("Light Meter pixAverage: {:.1f}".format(pixAverage))
